35add30b66443baf7899825673a273478a5bef8c
alex-dchlecht <alexschlecht@yandex.ru>
2023-06-24 21:52:59 +0300

    test commit

--- controllers/users.js
+++ controllers/users.js
@@ -76,7 +76,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-        }).send({});
+        }).send({ password: user.password });
     })
     .catch((err) => next(err));
 };
